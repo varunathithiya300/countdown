ce499b8a7b9d68a1abebd1fc4f06b254d30f66d9
Varun Athithiya <varunathithiya.shenbagaraj@octavebio.com>
2025-07-31 18:22:08 +0530

    changed formatting

--- countdowntimer.py
+++ countdowntimer.py
@@ -14,7 +14,7 @@ def showPopup():
     days_remaining = countDown(present.year, present.month, present.day)
     frm = ttk.Frame(root, padding=10)
     frm.grid()
-    ttk.Label(frm, text = str(days_remaining) + " days left @ Indium").grid(column=0, row=0)
+    ttk.Label(frm, text = " " + str(days_remaining) + " days left @ \n Indium Software").grid(column=0, row=0)
     ttk.Button(frm, text = "Close", command = root.destroy).grid(column=1, row=0)
     root.mainloop()
 
