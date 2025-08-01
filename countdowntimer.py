2e17d8eb8d3a74d455294bbc8211d9816502ceaa
Varun Athithiya <varunathithiya.shenbagaraj@octavebio.com>
2025-08-01 20:29:44 +0530

    reduced number of lines

--- countdowntimer.py
+++ countdowntimer.py
@@ -1,17 +1,12 @@
 from datetime import datetime, date
 import tkinter as tk
-from tkinter import messagebox, ttk
+from tkinter import ttk
 
-def countDown(year: int, month: int, day: int) -> int:
-    current_date = date(year, month, day)
-    last_working_day = date(2025, 9, 15)
-    delta_in_days = last_working_day - current_date
-    return delta_in_days.days
-
-def showPopup():
+def countDown():
     root = tk.Tk()
     present = datetime.now()
-    days_remaining = countDown(present.year, present.month, present.day)
+    last_working_day = date(2025, 9, 15)
+    days_remaining = (last_working_day - date(present.year, present.month, present.day)).days
     frm = ttk.Frame(root, padding=10)
     frm.grid()
     ttk.Label(frm, text = " " + str(days_remaining) + " days left @ \n Indium Software").grid(column=0, row=0)
@@ -19,4 +14,4 @@ def showPopup():
     root.mainloop()
 
 if __name__ == '__main__':
-    showPopup()
+    countDown()
